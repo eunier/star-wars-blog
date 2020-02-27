b77d7253f63adc8cff54233251c1e0d7dcddff4e
Yunier Alvarez <yunieralvarez@librem.one>
2020-02-26 19:02:12 -0500

    move character state to Home component

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,51 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import CharacterCard from './CharacterCard';
 
-const Character = () => {
-  const [characters, setCharacter] = useState([]);
-
-  useEffect(() => {
-    fetch('https://swapi.co/api/people')
-      .then(res => res.json())
-      .then(res =>
-        setCharacter(
-          res.results.map(character => ({
-            name: character.name,
-            gender: character.gender,
-            hairColor: character.hair_color,
-            eyeColor: character.eye_color,
-            height: character.height,
-            birthYear: character.birth_year,
-            skinColor: character.skin_color
-          }))
-        )
-      );
-  });
-
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h3>Characters</h3>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="d-flex flex-col flex-nowrap overflow-auto">
-          {characters.map((character, idx) => (
-            <CharacterCard
-              key={idx}
-              name={character.name}
-              gender={character.gender}
-              hairColor={character.hairColor}
-              eyeColor={character.eyeColor}
-            />
-          ))}
-        </div>
+const Characters = props => (
+  <div className="container">
+    <div className="row">
+      <div className="col">
+        <h3>Characters</h3>
       </div>
     </div>
-  );
+
+    <div className="row">
+      <div className="d-flex flex-col flex-nowrap overflow-auto">
+        {props.characters.map((character, idx) => (
+          <CharacterCard
+            key={idx}
+            name={character.name}
+            gender={character.gender}
+            hairColor={character.hairColor}
+            eyeColor={character.eyeColor}
+          />
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
+Characters.propTypes = {
+  characters: PropTypes.arrayOf(PropTypes.object)
 };
 
-export default Character;
+export default Characters;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Navbar from '../components/Navbar';
-import Character from '../components/Characters/Characters';
+import Characters from '../components/Characters/Characters';
 import Planets from '../components/Planets/Planets';
 
-const Home = () => (
-  <>
-    <Navbar />
-    <Character />
-    <Planets />
-  </>
-);
+const Home = () => {
+  const [characters, setCharacter] = useState([]);
+
+  useEffect(() => {
+    fetch('https://swapi.co/api/people')
+      .then(res => res.json())
+      .then(res =>
+        setCharacter(
+          res.results.map(character => ({
+            name: character.name,
+            gender: character.gender,
+            hairColor: character.hair_color,
+            eyeColor: character.eye_color,
+            height: character.height,
+            birthYear: character.birth_year,
+            skinColor: character.skin_color
+          }))
+        )
+      );
+  });
+
+  return (
+    <>
+      <Navbar />
+      <Characters characters={characters}/>
+      <Planets />
+    </>
+  );
+};
 export default Home;
